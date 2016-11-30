11c0ae0cc00afcd7becb8ded054e541ee4a7ab01
dracojoseph <dracojoseph@gmail.com>
2016-11-30 13:59:18 -0500

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -19,7 +19,14 @@ namespace Constants {
 	//Joysticks
 	static constexpr int PS4LeftJoystick = 1;
 	static constexpr int PS4RightJoystick = 5;
-	static constexpr int L2 = 3;
+	static constexpr int raiseArmButton = 0;//EDIT
+	static constexpr int lowerArmButton = 0;//EDIT
+	static constexpr int closeGripButton = 0;//EDIT
+	static constexpr int openGripButton = 0;//EDIT
+	static constexpr int releaseBigBallsButton = 0;//EDIT
+	static constexpr int releaseLittleBallsButton = 0;//EDIT
+	static constexpr int releaseAllButton = 0;//EDIT (do we need this?)
+	static constexpr int lockBallsButton = 0;//EDIT
 
 	//Arm
 	static constexpr int gripperInSole = 99;
